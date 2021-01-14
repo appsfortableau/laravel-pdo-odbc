996fa23833891c036bb9d596b493db8756444443
Yoram de Langen <yoramdelangen@gmail.com>
2021-01-14 16:23:24 +0100

    hacky solution for getting last id, some what dynamicly

--- src/Processors/SnowflakeProcessor.php
+++ src/Processors/SnowflakeProcessor.php
@@ -36,8 +36,9 @@ class SnowflakeProcessor extends Processor
 
         $connection->insert($sql, $values);
 
-        $result = $connection->selectOne('select last_query_id() as last_insert_id');
-        $id = $result->LAST_INSERT_ID ?? null;
+        $result = $connection->selectOne('select * from "'.$query->from.'" at(statement=>last_query_id())');
+        // hacky.... TODO we should fix this proper way...
+        $id = array_values((array) $result)[0];
 
         return is_numeric($id) ? (int) $id : $id;
     }
