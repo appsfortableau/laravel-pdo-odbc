8c464140ff72cec7031c42e4a41343327f25c317
Nicholas Zographos <nicholas.zographos@scripps.com>
2023-09-07 09:24:35 -0400

    Handle wrapping Expressions (so we can use raw() in selects)

--- src/Flavours/Snowflake/Concerns/GrammarHelper.php
+++ src/Flavours/Snowflake/Concerns/GrammarHelper.php
@@ -55,7 +55,7 @@ trait GrammarHelper
      */
     public function getValue($expression)
     {
-        return $expression instanceof Expression ? $expression->getValue() : $expression;
+        return $expression instanceof Expression ? $expression->getValue($this) : $expression;
     }
 
     /**
@@ -78,12 +78,17 @@ trait GrammarHelper
     /**
      * Wrap a single string in keyword identifiers.
      *
-     * @param string $value
+     * @param string | \Illuminate\Database\Query\Expression $column
      *
      * @return string
      */
     protected function wrapColumn($column)
     {
+
+        if (method_exists($this, 'isExpression') && $this->isExpression($column)) {
+            return $this->getValue($column);
+        }
+
         if ($column instanceof ColumnDefinition) {
             $column = $column->get('name');
         }
