97cda4d9ada5978f2839b244900a9019efecabf1
Nico van Zijl <nvz.solutions@gmail.com>
2026-03-25 14:27:26 +0100

    Added support for key pair authentication

--- src/Flavours/Snowflake/Connector.php
+++ src/Flavours/Snowflake/Connector.php
@@ -6,6 +6,8 @@ use Closure;
 use Exception;
 use LaravelPdoOdbc\Contracts\OdbcDriver;
 use LaravelPdoOdbc\ODBCConnector;
+use Illuminate\Support\Arr;
+
 use PDO;
 
 /**
@@ -25,6 +27,24 @@ class Connector extends ODBCConnector implements OdbcDriver
     {
         $connection = null;
         $usingSnowflakeDriver = $config['driver'] === 'snowflake_native';
+
+                // Handle Snowflake key-pair
+        if (Arr::get($config, 'authenticator') === 'key_pair') {
+            $privateKey = trim((string) Arr::get($config, 'private_key', ''));
+
+            if ($privateKey !== '') {
+                $config['priv_key_base64'] = base64_encode($privateKey);
+                $config['authenticator'] = 'SNOWFLAKE_JWT';
+                $config['odbc_driver'] = $config['odbcdriver'];
+
+                // Force ODBCConnector to build DSN dynamically
+                $allowedKeys = ['driver','server','database','warehouse','schema','port','priv_key_pwd','odbc_driver','authenticator','priv_key_base64', 'odbcdriver', 'username', 'options'];
+                $config = array_intersect_key($config, array_flip($allowedKeys));
+            }
+            else{
+                throw new Exception('Private key is required for key_pair authentication');
+            }
+        }
 
         // the PDO Snowflake driver was installed and the driver was snowflake, start using snowflake driver.
         if ($usingSnowflakeDriver) {
@@ -58,7 +78,7 @@ class Connector extends ODBCConnector implements OdbcDriver
             if (!env('SNOWFLAKE_DISABLE_FORCE_QUOTED_IDENTIFIER')) {
                 $connection->exec('ALTER SESSION SET QUOTED_IDENTIFIERS_IGNORE_CASE = false');
             }
-            
+
             // set default fetch mode for PDO
             $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $db->getFetchMode());
 
